d77770a16bcdc795e17e985365ae8cc371077bea
4015d2 <4015d2@grosella.ece.uprm.edu>
2017-02-08 14:25:15 -0400

    Le di uncomment a la segunda linea despues del main

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame Jerry = new JFrame("Hello, world!");
+		Jerry.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Jerry.setLocation(900, 450);
+		Jerry.setSize(200, 200);
+		Jerry.setVisible(true);
 	}
 }
