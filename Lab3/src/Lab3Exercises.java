8defc21c578ae703d36229a1a90ca43e74e25928
4015d2 <4015d2@grosella.ece.uprm.edu>
2017-02-08 14:28:59 -0400

    commit and push

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame Jerry = new JFrame("Hello, world!");
-		Jerry.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Jerry.setLocation(900, 450);
-		Jerry.setSize(200, 200);
-		Jerry.setVisible(true);
+		JFrame myframe = new JFrame("Jerry Bassat Castillo");
+		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myframe.setLocation(900, 450);
+		myframe.setSize(200, 200);
+		myframe.setVisible(true);
+	
 	}
 }
