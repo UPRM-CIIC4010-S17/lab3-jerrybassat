44fce2929b386795db571e4db67e3ba995adc15b
4015d2 <4015d2@grosella.ece.uprm.edu>
2017-02-08 14:59:18 -0400

    added color ns tudd

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,11 @@ public class Lab3Exercises {
 		JFrame myframe = new JFrame("Jerry Bassat Castillo");
 		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myframe.setLocation(900, 450);
+		
 		myframe.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+        myframe.getContentPane().add(myPanel);
 		myframe.setVisible(true);
 	
 	}
