bcbe1e694c4e0eda11bca62c2b3954496239270f
4015d2 <4015d2@grosella.ece.uprm.edu>
2017-02-08 15:20:52 -0400

    oval lab ended on question 7

---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+                        //Paint the background
+                        g.setColor(Color.CYAN);
+                        g.fillRect(x1, y1, width + 1, height + 1);
+                        //Draw a border
+//                        g.setColor(Color.red);
+//                        g.drawRect(x1, y1, width, height);
+//                    
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1 + 20, y1 + 20, width - 40, height -40);
+			
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        g.setColor(Color.black);
+//                        g.drawLine(x1+width ,y1 , x2-width, y2);
+                        
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval(x1 + (width/2), y1 + (height/2), 55, 55);
+			}
+			
+}
